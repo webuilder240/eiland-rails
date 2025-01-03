8853b59164ccce11d8cb75444644fbd7555c453e
webuilder240 <littlecub240@gmail.com>
2025-01-04 01:20:49 +0900

    fixed engine

--- lib/eiland/engine.rb
+++ lib/eiland/engine.rb
@@ -1,5 +1,8 @@
+require "rails/engine"
+
 module Eiland
   class Engine < ::Rails::Engine
+    isolate_namespace Eiland
     initializer "eiland_gem.action_controller" do
       ActiveSupport.on_load(:action_controller) do
         include Eiland::Helpers
