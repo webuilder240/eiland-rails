d85904cd1fdbf74b809de2bc31e5cc4a29c3fb0e
webuilder240 <littlecub240@gmail.com>
2025-01-04 01:26:24 +0900

    fixed engine

--- lib/eiland-rails.rb
+++ lib/eiland-rails.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 require 'eiland/engine'
+require 'eiland/helpers'
 require_relative "eiland/version"
--- lib/eiland/engine.rb
+++ lib/eiland/engine.rb
@@ -5,8 +5,8 @@ module Eiland
   class Engine < ::Rails::Engine
     isolate_namespace Eiland
     initializer "eiland_gem.action_controller" do
-      ActiveSupport.on_load(:action_controller) do
-        include Eiland::Helpers
+      ActiveSupport.on_load(:action_controller_base) do
+        helper Eiland::Helpers
       end
     end
   end
